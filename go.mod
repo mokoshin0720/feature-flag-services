ce8ea285397bd2be7a52310d82d27c46a863a04f
mokoshin0720 <shinya.prv@gmail.com>
2025-05-29 09:41:00 +0900

    初回コミットによる設定ファイルとメインプログラムの追加

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/mokoshin0720/feature-flag-services
+
+go 1.24.3
+
+require (
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/gregjones/httpcache v0.0.0-20171119193500-2bcd89a1743f // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/launchdarkly/ccache v1.1.0 // indirect
+	github.com/launchdarkly/eventsource v1.8.0 // indirect
+	github.com/launchdarkly/go-jsonstream/v3 v3.1.0 // indirect
+	github.com/launchdarkly/go-sdk-common/v3 v3.1.0 // indirect
+	github.com/launchdarkly/go-sdk-events/v3 v3.5.0 // indirect
+	github.com/launchdarkly/go-semver v1.0.3 // indirect
+	github.com/launchdarkly/go-server-sdk-evaluation/v3 v3.0.1 // indirect
+	github.com/launchdarkly/go-server-sdk/v7 v7.10.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
+	golang.org/x/sync v0.8.0 // indirect
+)
---
+++ launch_darkly/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/launchdarkly/go-sdk-common/v3/ldcontext"
+	"github.com/launchdarkly/go-sdk-common/v3/ldvalue"
+	ld "github.com/launchdarkly/go-server-sdk/v7"
+)
+
+  func showBanner() {
+    fmt.Print("\n        ██       \n" +
+      "          ██     \n" +
+      "      ████████   \n" +
+      "         ███████ \n" +
+      "██ LAUNCHDARKLY █\n" +
+      "         ███████ \n" +
+      "      ████████   \n" +
+      "          ██     \n" +
+      "        ██       \n")
+  }
+
+  func showMessage(s string) { fmt.Printf("*** %s\n\n", s) }
+
+  func main() {
+    var sdkKey = os.Getenv("LAUNCHDARKLY_SDK_KEY")
+
+    if sdkKey == "" {
+      showMessage("LaunchDarkly SDK key is required: set the LAUNCHDARKLY_SDK_KEY environment variable and try again.")
+      os.Exit(1)
+    }
+
+    ldClient, _ := ld.MakeClient(sdkKey, 5*time.Second)
+    if ldClient.Initialized() {
+      showMessage("SDK successfully initialized!")
+    } else {
+      showMessage("SDK failed to initialize")
+      os.Exit(1)
+    }
+
+    // Set up the evaluation context. This context should appear on your LaunchDarkly contexts dashboard
+    // soon after you run the demo.
+    context := ldcontext.NewBuilder("example-user-key").
+      Name("Sandy").
+      Build()
+
+    // Set featureFlagKey to the feature flag key you want to evaluate.
+    var featureFlagKey = "sample-in-private"
+
+    if os.Getenv("LAUNCHDARKLY_FLAG_KEY") != "" {
+      featureFlagKey = os.Getenv("LAUNCHDARKLY_FLAG_KEY")
+    }
+
+    flagValue, err := ldClient.BoolVariation(featureFlagKey, context, false)
+    if err != nil {
+      showMessage("error: " + err.Error())
+    }
+
+    showMessage(fmt.Sprintf("The '%s' feature flag evaluates to %t.", featureFlagKey, flagValue))
+
+    if flagValue {
+      showBanner()
+    }
+
+    if os.Getenv("CI") != "" {
+      os.Exit(0)
+    }
+
+    updateCh := ldClient.GetFlagTracker().AddFlagValueChangeListener(featureFlagKey, context, ldvalue.Null())
+
+    for event := range updateCh {
+      showMessage(fmt.Sprintf("The '%s' feature flag evaluates to %t.", featureFlagKey, event.NewValue.BoolValue()))
+      if event.NewValue.BoolValue() {
+        showBanner()
+      }
+    }
+  }
